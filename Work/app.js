58a5383278a1d883b2a82c4514bbac32e36656be
GirlsTeam <dashaisfunny@yandex.ru>
2015-12-23 10:55:25 +0500

    signin, signup

--- Work/app.js
+++ Work/app.js
@@ -2,22 +2,27 @@ var app = require('express')();
 var express = require('express')
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var client = require('./client.js');
+var mongo = require('mongodb');
 
 clientid=1;
 
 app.use(express.static('public'));
 
+
 app.get('/wait', function(req, res){
   res.sendfile(__dirname + '/public/waiting.html');
 });
 
-app.get('/main', function(req, res){
+app.get('/', function(req, res){
   res.sendfile(__dirname + '/public/main.html');
 });
 
 app.get('/play', function(req, res){
   res.sendfile(__dirname + '/public/indexf.html');
+});
+
+app.get('/fail', function(req, res){
+  res.sendfile(__dirname + '/public/fail.html');
 });
 
 clientid = 1
@@ -37,7 +42,7 @@ io.on('connection', function(socket){
     
   socket.on('disconnect', function(){
     socket.emit('user disconnected');
-    console.log('user disconected' + socket.id)
+    console.log('user disconected: ' + socket.id)
   });
     
   socket.on('usercome', function(socket){
@@ -57,6 +62,57 @@ io.on('connection', function(socket){
           }
     });
     
+});
+
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+var mongo_connection = 'mongodb://127.0.0.1:27017/bubbles';
+var db;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+MongoClient.connect(mongo_connection, function(err, database) {
+    if (err) throw err;
+    db = database;
+    db.collection("users").ensureIndex("user", {unique: true}, function(err, index) {
+    if (err) throw err;
+    });
+})
+
+app.post('/signup', function(req, res, next) {
+    var collection = db.collection("users");
+    var user = req.body.user || "";
+    var passwd = req.body.password || "";
+    console.log(req.body);
+    if (!user || !passwd)
+        return res.redirect('/fail');
+    
+    collection.insertOne({'user': user, 'passwd': passwd}, function(err, result) {
+        console.log('try to insert to the database ');
+        if (err)
+            return res.redirect('/fail');
+        res.redirect('/wait');
+    });
+});
+
+app.post('/signin', function(req, res, next) {
+    var collection = db.collection("users");
+    var user = req.body.user || "";
+    var passwd = req.body.password || "";
+    console.log(req.body);
+    if (!user || !passwd)
+        return res.redirect('/fail');
+
+    collection.findOne({'user': user}, function(err, item) {
+        console.log(item)
+        if (err || !item)
+            return res.redirect('/fail');
+        if (item.passwd != passwd)
+            return res.redirect('/fail');
+        res.redirect('/wait');
+    });
 });
 
 
